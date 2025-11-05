c512785f1ba2995a39e09551a5f75974cc00226b
Szymon Sobolewski <sigma@gmail.com>
2025-11-05 09:00:03 +0100

    sa

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -15,11 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.shortcuts import redirect
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
     path("polls/", include("polls.urls")),
-    path('', lambda request: redirect('polls/')),
+    path("admin/", admin.site.urls),
 ]
